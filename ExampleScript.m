6856a7552386bd1b14896fb8c0752241f362cb74
Marlene <marlene.poncet@gmail.com>
2022-07-01 22:17:14 +0100

    more intuitive simulation with 32 chan & noise

--- ExampleScript.m
+++ ExampleScript.m
@@ -1,46 +1,47 @@
 % Example script 
 % This script simulates activity over time in V1 in condition 1 and hMT+ in
-% condition 2 for a montage with 16 electrodes. 
-% Then, templates corresponding to the 16 electrodes montage are created  
+% condition 2 for a montage with 32 electrodes. 
+% Then, templates corresponding to the 32 electrodes montage are created  
 % using createCustomTemplates. The activity is recovered from functional 
-% brain areas using fitEEGTemplates and plotted for the 2 conditions
-% separately. 
+% brain areas using fitEEGTemplates. 
 % If it works, you should see some figures showing the alignment of the
-% electrodes with the specified 16 electrodes montage and the retrieved
-% activity in the 18 functional brain areas (over time). Note that some
-% activity is retrieved in non-active areas probably due to the very small
-% number of electrodes used here (16) but the max activity is retrieved in
-% the correct areas. 
+% electrodes with the specified 32 electrodes montage. 
+% The last 2 figures should look similar. One is the simulated activity,
+% the other one is the retrieved activity over time (with noise) in the 18 
+% functional brain areas for the 2 conditions (in blue and red).  
 
-% for cellfind to work outside the function (or when running the script one
-% line at a time), needs to add it to path! 
 
 %% simulate activity for 2 conditions 
 % (corresponding to the EEG average across participants in 2 conditions)
 load(['templates' filesep 'template_Standard_1005.mat'])
-% use a montage with 16 electrodes
-setOfChannels = {'Fp1' 'Fp2' 'F4' 'Fz' 'F3' 'T7' 'C3' 'Cz'...
-    'C4' 'T8' 'P4' 'Pz' 'P3' 'O1' 'Oz' 'O2'};
+% use a montage with 32 electrodes
+setOfChannels = {'Fp1' 'AF3' 'F7' 'F3' 'FC1' 'FC5' 'T7' 'C3' 'CP1' 'CP5'...
+    'P7' 'P3' 'Pz' 'PO3' 'O1' 'Oz' 'O2' 'PO4' 'P4' 'P8' 'CP6' 'CP2' 'C4'...
+    'T8' 'FC6' 'FC2' 'F4' 'F8' 'AF4' 'Fp2' 'Fz' 'Cz'};
 % get indexes corresponding to this set of channels from the templates
 matchIndex = cell2mat(arrayfun(@(x) cellfind(templates1005.label,setOfChannels{x}),1:length(setOfChannels),'uni',false));
 
-% topography for activation in bilateral V1  
-topo1 = sum(templates1005.weights(matchIndex,[1 2]),2); 
-% topography for activation in bilateral hMT+
-topo2 = sum(templates1005.weights(matchIndex,[17 18]),2); 
+cond1ROI = [1 2]; % bilateral V1 active  
+cond2ROI = [17 18]; % bilateral hMT+ active 
 % activation over time using half cosine
 % 90 timepoints, peak activation at 45  
 cosFilt = cos(-pi:pi/45:pi-pi/45);
 cosFilt(cosFilt<0) = 0;
-dataCond1 = topo1 .* cosFilt;
-dataCond2 = topo2 .* cosFilt;
+simulCond = zeros(size(templates1005.listROIs,2),length(cosFilt),2);
+simulCond(cond1ROI,:,1) = repmat(cosFilt,length(cond1ROI),1);
+simulCond(cond2ROI,:,2) = repmat(cosFilt,length(cond1ROI),1);
 
-% get simulated data over time for plotting
-simulatedData = zeros(size(templates1005.listROIs,2),length(cosFilt),2);
-simulatedData(1,:,1) = cosFilt;
-simulatedData(2,:,1) = cosFilt;
-simulatedData(17,:,2) = cosFilt;
-simulatedData(18,:,2) = cosFilt;
+% add noise (needs different regularisation when no noise)
+simulCond = simulCond + randn(size(simulCond))*0.1;
+
+% simulate data over electrodes
+dataCond1 = templates1005.weights(matchIndex,:) * simulCond(:,:,1);
+dataCond2 = templates1005.weights(matchIndex,:) * simulCond(:,:,2);
+
+% reference data to average 
+dataCond1 = bsxfun(@minus,dataCond1, mean(dataCond1));
+dataCond2 = bsxfun(@minus,dataCond2, mean(dataCond2));
+
 
 % read channel locations for the simulated activity
 % in EEGLAB can be loaded from a file using readlocs or can look up 
@@ -56,24 +57,41 @@ simulatedData(18,:,2) = cosFilt;
 channelInfo.label = setOfChannels;
 % order should follow the order of the channel labels
 channelInfo.chanpos = [-27	83	-3
-    27	83	-3
-    48	59	44
-    0	63	61
-    -48	59	44
-    -87	0	-3
-    -63	0	61
-    0	0	88
-    63	0	61
-    87	0	-3
-    48	-59	44
-    0	-63	61
-    -48	-59	44
-    -27	-83	-3
-    0	-87	-3
-    27	-83	-3];
+-36	76	24
+-71	51	-3
+-48	59	44
+-33	33	74
+-78	30	27
+-87	0	-3
+-63	0	61
+-33	-33	74
+-78	-30	27
+-71	-51	-3
+-48	-59	44
+0	-63	61
+-36	-76	24
+-27	-83	-3
+0	-87	-3
+27	-83	-3
+36	-76	24
+48	-59	44
+71	-51	-3
+78	-30	27
+33	-33	74
+63	0	61
+87	0	-3
+78	30	27
+33	33	74
+48	59	44
+71	51	-3
+36	76	24
+27	83	-3
+0	63	61
+0	0	88];
 
 %% create templates corresponding to the montage
-% the following uses an average reference. 
+% the following uses an average reference (0), which match the reference of
+% the data
 % Try myTemplates = createCustomTemplates(channelInfo) for other options or
 % read the help in the function
 myTemplates = createCustomTemplates(channelInfo,0); 
@@ -81,8 +99,8 @@ myTemplates = createCustomTemplates(channelInfo,0);
 %% fit data
 % data simulated with 90 timepoints, peak activation half way (45)
 % in 2 conditions (one with V1 activation, the other with hMT+)
-% put the 2 conditions in the save variable (simulation is the ERP of an
-% average of multiple participants)
+% put the 2 conditions in the save variable (assumes that the simulated
+% data is an average of multiple participants)
 averageData(:,:,1) = dataCond1;
 averageData(:,:,2) = dataCond2;
 % for a cell it would be: averageData{1} = dataCond1; averageData{2} = dataCond2;
@@ -91,11 +109,11 @@ areaActive = fitEEGTemplates(averageData,myTemplates);
 %% plot
 figure; hold on
 loc = [1:9;10:18]; loc = loc(:);
-for area=1:size(areaActive,1)
+for area=1:length(templates1005.listROIs)
     subplot(2,9,loc(area)); hold on
-    plot(simulatedData(area,:,1))
-    plot(simulatedData(area,:,2))
-    title(myTemplates.listROIs(area))
+    plot(simulCond(area,:,1))
+    plot(simulCond(area,:,2))
+    title(templates1005.listROIs(area))
     ylim([-1 1])
     xlabel('time')
 end
@@ -103,7 +121,7 @@ legend('cond 1 simulates V1', 'cond 2 simulates hMT+','location','best')
 
 figure; hold on
 loc = [1:9;10:18]; loc = loc(:);
-for area=1:size(areaActive,1)
+for area=1:length(templates1005.listROIs)
     subplot(2,9,loc(area)); hold on
     plot(areaActive(area,:,1))
     plot(areaActive(area,:,2))
@@ -111,4 +129,4 @@ for area=1:size(areaActive,1)
     ylim([-1 1])
     xlabel('time')
 end
-legend('cond 1 retrives V1', 'cond 2 retrieves hMT+','location','best')
+legend('cond 1 retrieves V1', 'cond 2 retrieves hMT+','location','best')
